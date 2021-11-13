ef6b4d003e5f9f59373ecf8bc3ea8bd504655e9f
富名腰美樹 <l.fnks33@gmail.com>
2021-11-13 22:54:22 +0900

    豚　鳴き声成功クリックすると泣きます

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,47 @@
-$(".buta").on("click",function(){
-    location.href="https://youtu.be/6zbsUtQL4nY"
-})
-$(".kaba").on("click",function(){
-    location.href="https://honcierge.jp/articles/shelf_story/6258"
-})
-$(".koara").on("click",function(){
-    location.href="https://natgeo.nikkeibp.co.jp/nng/article/20141218/428831/"
-})
-$(".simauma").on("click",function(){
-    location.href="https://ja.wikipedia.org/wiki/%E3%82%B7%E3%83%9E%E3%82%A6%E3%83%9E"
-})
-$(".zou").on("click",function(){
-    location.href="https://www.wwf.or.jp/activities/basicinfo/4291.html"
-})
-$(".hukurou").on("click",function(){
-    location.href="https://youtu.be/rC2CmJtR5vI"
-})
-$(".hebi").on("click",function(){
-    location.href="https://www.pref.okinawa.lg.jp/site/hoken/eiken/eisei/habuhebinotokucyo.html"
-})
-$(".penginn").on("click",function(){
-    location.href="https://youtu.be/7HD1jIztbx0"
-})
-$(".rakuda").on("click",function(){
-    location.href="https://honcierge.jp/articles/shelf_story/6147"
-})
-$(".risu").on("click",function(){
-    location.href="https://youtu.be/wJ9zeh-k2hE"
-})
-$(".saru").on("click",function(){
-    location.href="https://youtu.be/fh6OUgfrG_U"
-})
+// $(".buta").on("click",function(){
+//     location.href="https://youtu.be/6zbsUtQL4nY"
+// })
+// $(".kaba").on("click",function(){
+//     location.href="https://honcierge.jp/articles/shelf_story/6258"
+// })
+// $(".koara").on("click",function(){
+//     location.href="https://natgeo.nikkeibp.co.jp/nng/article/20141218/428831/"
+// })
+// $(".simauma").on("click",function(){
+//     location.href="https://ja.wikipedia.org/wiki/%E3%82%B7%E3%83%9E%E3%82%A6%E3%83%9E"
+// })
+// $(".zou").on("click",function(){
+//     location.href="https://www.wwf.or.jp/activities/basicinfo/4291.html"
+// })
+// $(".hukurou").on("click",function(){
+//     location.href="https://youtu.be/rC2CmJtR5vI"
+// })
+// $(".hebi").on("click",function(){
+//     location.href="https://www.pref.okinawa.lg.jp/site/hoken/eiken/eisei/habuhebinotokucyo.html"
+// })
+// $(".penginn").on("click",function(){
+//     location.href="https://youtu.be/7HD1jIztbx0"
+// })
+// $(".rakuda").on("click",function(){
+//     location.href="https://honcierge.jp/articles/shelf_story/6147"
+// })
+// $(".risu").on("click",function(){
+//     location.href="https://youtu.be/wJ9zeh-k2hE"
+// })
+// $(".saru").on("click",function(){
+//     location.href="https://youtu.be/fh6OUgfrG_U"
+// })
+
+
+$(function(){
+ 
+	//音を鳴らす
+	$('.buta').on("click",function(){
+ 
+		document.getElementById("overSound").currentTime = 0;
+		document.getElementById("overSound").play();
+ 
+	});
+ 
+ 
+});
